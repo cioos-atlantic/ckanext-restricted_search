a2fecd576a5e9e0f4ce1a799d395fe47452b6915
JaredMclellan <jaredamclellan@gmail.com>
2022-05-17 11:06:32 -0300

    Initial commit based on work from the Vitality plugin

---
+++ ckanext/restricted_search/plugin.py
@@ -0,0 +1,86 @@
+import logging
+
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+class RestrictedSearchPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic',
+            'restricted_search')
+
+
+    def before_search(self, search_params):
+        facet_query = search_params['fq']
+        if 'restricted_search:"enabled"' in facet_query:
+            facet_query = facet_query.replace('restricted_search:"enabled"', "")
+            if "eov:" in facet_query or 'tags_en:' in facet_query or 'tags:' in facet_query:
+                fq_split = facet_query.split(' ')
+                final_query = ""
+                for x in fq_split:
+                    if(x.startswith('eov:') and '"' in x):
+                        eov = x.split('"')[1]
+                        eov_restricted = 'res_extras_eov_restricted:"' + eov + '"'
+                        x= '(eov:"' + eov + '" OR ' + eov_restricted + ')'
+                    elif(x.startswith('tags:') and '"' in x):
+                        tags = x.split('"')[1]
+                        tags_restricted = 'res_extras_keywords_restricted:"' + tags + '"'
+                        x= '(tags:"' + tags + '" OR ' + tags_restricted + ')'
+                    elif(x.startswith('tags_en:') and '"' in x):
+                        tags = x.split('"')[1]
+                        tags_restricted = 'res_extras_keywords_restricted:"' + tags + '"'
+                        x= '(tags_en:"' + tags + '" OR ' + tags_restricted + ')'
+                    elif(x.startswith('tags_fr:') and '"' in x):
+                        tags = x.split('"')[1]
+                        tags_restricted = 'res_extras_keywords_restricted:"' + tags + '"'
+                        x= '(tags_fr:"' + tags + '" OR ' + tags_restricted + ')'
+                    final_query += x + ' '
+                search_params['fq'] = final_query.strip()
+            else:
+                search_params['fq'] = facet_query.strip()   
+        return search_params
+
+
+    def after_search(self, search_results, search_params):
+        # Gets the current user's ID (or if the user object does not exist, sets user as 'public')
+        
+        datasets = search_results['results']
+        
+        # Checks if the search requires restricted variable checking
+        # TODO Overall costly - find a better alternative
+        restricted_search_enabled = False
+        restricted_search_eovs = []
+        restricted_search_keywords = []
+        for x in search_params['fq'][0].replace(")","").replace("(", "").split(" "):
+            if(x.startswith('res_extras_eov_restricted')):
+                restricted_search_eovs.append(x.split('"')[1])
+                restricted_search_enabled = True
+            elif(x.startswith('res_extras_keywords_restricted')):
+                restricted_search_keywords.append(x.split('"')[1])
+                restricted_search_enabled = True
+
+        # Go through each of the datasets returned in the results
+        for x in range(len(datasets)):
+            pkg_dict = search_results['results'][x]
+            if restricted_search_enabled:
+                try:
+                    if('res_extras_eov_restricted' in pkg_dict):
+                        for x in restricted_search_eovs:
+                            if x in pkg_dict['res_extras_eov_restricted']:
+                                pkg_dict['mark_restricted'] = True
+                                continue
+                    if('res_extras_keywords_restricted' in pkg_dict and 'mark_restricted' not in pkg_dict):
+                        for x in restricted_search_keywords:
+                            if x in pkg_dict['res_extras_keywords_restricted']['en'] or x in pkg_dict['res_extras_keywords_restricted']['fr']:
+                                pkg_dict['mark_restricted'] = True
+                                continue
+                except:
+                    log.info('An error with restricted search occurred')
+        return search_results
+    
